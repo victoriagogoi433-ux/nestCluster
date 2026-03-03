f1b6067fa731ae438e531339762673eea8d3487d
Sohangi Babbar <sohangibabbar4@gmail.com>
2026-03-03 17:00:19 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleVertical(id) {
-            const content = document.getElementById(`content-${id}`);
-            const arrow = document.getElementById(`arrow-${id}`);
-            content.classList.toggle('hidden');
-            arrow.classList.toggle('rotate-180');}
-            
